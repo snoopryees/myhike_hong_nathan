484a6d98ce72cd3e7e14d87dfeb0181365c2caee
Nathan Hong <nhong11@my.bcit.ca>
2025-03-04 10:44:09 -0800

    finished demo09

--- scripts/main.js
+++ scripts/main.js
@@ -23,3 +23,94 @@ function getNameFromAuth() {
     });
 }
 getNameFromAuth(); //run the function
+
+// Function to read the quote of the day from the Firestore "quotes" collection
+// Input param is the String representing the day of the week, aka, the document name
+function readQuote(day) {
+    db.collection("quotes").doc(day)                                                         //name of the collection and documents should matach excatly with what you have in Firestore
+        .onSnapshot(dayDoc => {                                                              //arrow notation
+            console.log("current document data: " + dayDoc.data());                          //.data() returns data object
+            document.getElementById("quote-goes-here").innerHTML = dayDoc.data().quote;      //using javascript to display the data on the right place
+
+            //Here are other ways to access key-value data fields
+            //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
+            //$("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
+            //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
+
+        }, (error) => {
+            console.log("Error calling onSnapshot", error);
+        });
+}
+readQuote("tuesday");        //calling the function
+
+function writeHikes() {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var hikesRef = db.collection("hikes");
+
+    hikesRef.add({
+        code: "BBY01",
+        name: "Burnaby Lake Park Trail", //replace with your own city?
+        city: "Burnaby",
+        province: "BC",
+        level: "easy",
+        details: "A lovely place for lunch walk",
+        length: 10,          //number value
+        hike_time: 60,       //number value
+        lat: 49.2467097082573,
+        lng: -122.9187029619698,
+        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+    });
+    hikesRef.add({
+        code: "AM01",
+        name: "Buntzen Lake Trail", //replace with your own city?
+        city: "Anmore",
+        province: "BC",
+        level: "moderate",
+        details: "Close to town, and relaxing",
+        length: 10.5,      //number value
+        hike_time: 80,     //number value
+        lat: 49.3399431028579,
+        lng: -122.85908496766939,
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
+    });
+    hikesRef.add({
+        code: "NV01",
+        name: "Mount Seymour Trail", //replace with your own city?
+        city: "North Vancouver",
+        province: "BC",
+        level: "hard",
+        details: "Amazing ski slope views",
+        length: 8.2,        //number value
+        hike_time: 120,     //number value
+        lat: 49.38847101455571,
+        lng: -122.94092543551031,
+        last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 1, 2023"))
+    });
+}
+
+function displayHikes() {
+    let cardTemplate = document.getElementById("hikeCardTemplate");
+    let hikeCardGroup = document.getElementById("hikes-go-here");
+
+    db.collection("hikes").get()
+        .then(allHikes => {
+            allHikes.forEach(doc => {
+                let newcard = cardTemplate.content.cloneNode(true);
+                var docID = doc.id;
+
+                //update title and text and other elements
+                newcard.querySelector('.card-title').innerHTML = doc.data().name;
+                newcard.querySelector('.card-length').innerHTML = doc.data().length + " km";
+                newcard.querySelector('.card-text').innerHTML = doc.data().details;
+                newcard.querySelector('.card-image').src = `./images/${doc.data().code}.jpg`;  // Using card-image class
+                newcard.querySelector('.card-href').href = "review.html";  // Using card-href classF
+                newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
+
+                //add the card to the container
+                hikeCardGroup.appendChild(newcard);
+            });
+        })
+}
+
+// Call this function when the page loads
+displayHikes();
